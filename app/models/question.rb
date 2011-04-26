6fb80c26242e204eeaeee2e05deead0caae926f4
liyulong <l_y_l12@126.com>
2011-04-26 16:05:42 +0800

    add model question,question_attr,question_tag_relation,paper --modify by liyulong

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+  has_many:question_tags
+  belongs_to:question_categories
+  has_many:question_tag_relations, :foreign_key =>"question_id"
+  has_many:block_question_relations, :foreign_key => "question_id"
 end
