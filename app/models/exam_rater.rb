f84bc4af7406f11ca0bbcd34c4052495986d2c0c
jeffrey <jeffrey6052@163.com>
2011-04-26 15:17:05 +0800

    modify 'exam_user','exam_record' ---jeffrey

--- app/models/exam_rater.rb
+++ app/models/exam_rater.rb
@@ -1,4 +1,5 @@
 class ExamRater < ActiveRecord::Base
   belongs_to :exam_plan
   has_many :rater_record_relations
+  has_many :exam_records,:through=>:rater_record_relations,:foreign_key=>:exam_record_id
 end
