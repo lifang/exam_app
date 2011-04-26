1ef5352ffd42254446b35f652090d51f8a05aec3
liyulong <l_y_l12@126.com>
2011-04-26 16:26:49 +0800

    modify model question --modify by liyulong

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
-  has_many:question_tags
+  has_many:question_tags 
   belongs_to:question_categories
-  has_many:question_tag_relations, :foreign_key =>"question_id"
-  has_many:block_question_relations, :foreign_key => "question_id"
+  has_many:question_tag_relations
+  has_many:block_question_relations
 end
