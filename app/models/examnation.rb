d17031213ba4e024acdbcb2394efd0ca9bbfd421
qianjun <er6788@163.com>
2011-04-26 17:07:04 +0800

    merge --modify by qianjun

--- app/models/examnation.rb
+++ app/models/examnation.rb
@@ -1,4 +1,8 @@
 class Examnation < ActiveRecord::Base
+<<<<<<< HEAD
 has_many:exam_plans,:dependent => :destroy
+=======
+  has_many:exam_plans,:dependent => :destroy
+>>>>>>> e29318efc1aad037da78d5493b77a68b7df9f4e2
   belongs_to:paper,:dependent => :destroy
 end
