e1b3016318103b2afdff6ed3ebfebede9a83edec
lifang <santhe_2004@126.com>
2011-04-27 16:15:43 +0800

    modify migrations --by lifang

--- db/migrate/20110426054234_create_rater_record_relations.rb
+++ db/migrate/20110426054234_create_rater_record_relations.rb
@@ -3,8 +3,6 @@ class CreateRaterRecordRelations < ActiveRecord::Migration
     create_table :rater_record_relations do |t|
       t.integer :exam_record_id
       t.integer :exam_rater_id
-
-      t.timestamps
     end
     add_index :rater_record_relations,:exam_record_id
     add_index :rater_record_relations,:exam_rater_id
