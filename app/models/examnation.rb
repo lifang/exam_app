0868ac1ee13d48e7169625606338d19f21bc3eb3
jeffrey <jeffrey6052@163.com>
2011-04-27 15:18:26 +0800

    jeffrey

--- app/models/examnation.rb
+++ app/models/examnation.rb
@@ -1,5 +1,6 @@
 class Examnation < ActiveRecord::Base
-  has_many:exam_plans,:dependent => :destroy
-  belongs_to:paper,:dependent => :destroy
+  has_many :exam_plans,:dependent => :destroy
+  belongs_to :paper,:dependent => :destroy
+  belongs_to :user
 
 end
--- app/models/paper_block.rb
+++ app/models/paper_block.rb
@@ -2,6 +2,8 @@ class PaperBlock < ActiveRecord::Base
    has_many :block_question_relations,:dependent => :destroy
    belongs_to :paper
    has_many :question,:through=>:block_question_relations,:foreign_key=>:question_id
+
+  BLOCK_TYPE = {:SINGLE_CHOOSE =>1, :MORE_CHOOSE =>2}
 end
 
 
