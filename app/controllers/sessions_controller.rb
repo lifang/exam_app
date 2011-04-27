b548cd8d8c453c30b8515104c9e5a5af3aa30e19
hejianfeng <marshjf5@126.com>
2011-04-27 14:24:18 +0800

    foreign_key --modity by hejianfeng

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def new
-    
+    user = User.authenticate(params[:session][:email], params[:session][:password])
+    if user.nil?
+     # flash[:error] = "您输入的邮箱或者密码错误，请重新输入。"
+      render 'new'
+    else
+      session[:user_email]= params[:session][:email]
+      redirect_to "/users/#{user.id}"
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      
+      :email => "user@example.com",
+      :password => "user"
+      
+    }
+  end
+  
+  describe "password validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+  end
+  it "should require a password confirmation"  do
+		User.new(@attr.merge(:password => "invalid")).should_not be_valid
+
+	end
+  it "should reject short passwords" do
+		short = "a" * 5
+		User.new(@attr.merge(:password => short, :password_confirmation => short)).should_not be_valid
+	end
+  it "should reject long passwords" do
+		long = "a" * 41
+		User.new(@attr.merge(:password => long, :password_confirmation => long)).should_not be_valid
+	end
+
+  describe "user_name validations" do
+    it "name should be presence" do
+      user=User.new(@attr.merge(:name=>""))
+      user.should_not be_valid
+    end
+  end
 end
