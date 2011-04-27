93102acf9f3dd9cca0269beae084722fe263635c
lifang <santhe_2004@126.com>
2011-04-27 14:39:17 +0800

    add factory --by lifang

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+Factory.define :user do |user|
+	user.name "test"
+  user.user_name "test"
+	user.email "test@gmail.com"
+	user.password "lifang"
+	user.password_confirmation "lifang"
+  user.mobilephone "12345678909"
+  user.address "beijin"
+end
