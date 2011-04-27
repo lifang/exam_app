3ff154b0893af28498f1d612da23e199baa11039
hejianfeng <marshjf5@126.com>
2011-04-27 17:53:14 +0800

    first_day --modity by hejianfeng

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
     
   end
 
-  def new
-    user = User.authenticate(params[:session][:email], params[:session][:password])
-    if user.nil?
+  def create
+   @user=User.find_by_user_name(params[:session][:user_name])
+    if @user.nil?
      # flash[:error] = "您输入的邮箱或者密码错误，请重新输入。"
-      render 'new'
+      render '/sessions/new'
     else
-      session[:user_email]= params[:session][:email]
-      redirect_to "/users/#{user.id}"
-    end
+      session[:user_name]=@user.user_name
+      session[:user_password]=@user.password
+      redirect_to "/users/#{@user.id}"
+   end
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe SessionsController do
   render_views
-  describe "POST'new'"do
+  describe "POST'create'"do
     it "should be right page" do
       @attr={:email=>"",:password=>""}
-      post:new,:session=>@attr
+      post:create,:session=>@attr
       response.should be_success
     end
     it"should be the right page"do
       @attr={:email=>"1111@126.com",:password=>"11"}
-      post:new,:session=>@attr
+      post:create,:session=>@attr
       response.should be_success
     end
     it"should be the right page"do
        @attr={:email=>"",:password=>""}
-	     post:new,:session=>@attr
+	     post:create,:session=>@attr
        response.should render_template('new')
     end
     end
