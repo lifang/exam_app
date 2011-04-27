f321309d4f6fd485feb07aa6bef07c7fc7f8f877
lifang <santhe_2004@126.com>
2011-04-27 20:44:35 +0800

    add common method --by lifnag

--- app/models/paper_block.rb
+++ app/models/paper_block.rb
@@ -1,7 +1,7 @@
 class PaperBlock < ActiveRecord::Base
    has_many :block_question_relations,:dependent => :destroy
    belongs_to :paper
-   has_many :question,:through=>:block_question_relations,:foreign_key=>:question_id
+   has_many :questions,:through=>:block_question_relations,:foreign_key=>:question_id
 
   BLOCK_TYPE = {:SINGLE_CHOOSE =>1, :MORE_CHOOSE =>2}
 end
