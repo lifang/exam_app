6fc9f4958f2b6b6b039903a54a88431d283614cc
hejianfeng <marshjf5@126.com>
2011-04-29 15:23:55 +0800

    modity by

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,6 +1,6 @@
 class PapersController < ApplicationController
 
-<<<<<<< HEAD
+
 
   def new
     
@@ -9,20 +9,19 @@ class PapersController < ApplicationController
     Paper.find(params[:id]).destroy
     redirect_to "/papers"
   end
-=======
->>>>>>> 4de4cb4fdf9c1b950d285a202690bfc3012614be
+
   def show
     @paper=Paper.find(params[:id])
     @block1=PaperBlock.find(1)     #修改
     @block2=PaperBlock.find(2)     #修改
   end
-<<<<<<< HEAD
+
   def create
     Paper.create(:paper_category_id=>"1",:title=>params[:paper][:paper_title],:description=>params[:paper][:paper_describe],:creater_id=>"#{User.find_by_name(cookies[:user_name]).id}",:total_score=>params[:paper][:paper_total_score],:total_question_num=>params[:paper][:paper_total_question_num])
-=======
+
 
   def new_step_one
->>>>>>> 4de4cb4fdf9c1b950d285a202690bfc3012614be
+
     
   end
 
@@ -46,8 +45,7 @@ class PapersController < ApplicationController
   end
   
   def index
-    cookies[:user_id] = 1
-    @papers=Paper.find_by_sql("select * from papers p where p.creater_id=#{cookies[:user_id]}").paginate(:per_page =>10, :page => params[:page],:order => "created_at desc")
+    @papers=Paper.find_by_sql("select * from papers p where p.creater_id=#{ cookies[:user_id]}").paginate(:per_page =>10, :page => params[:page],:order => "created_at desc")
   end
 
   def new
@@ -58,5 +56,6 @@ class PapersController < ApplicationController
     if User.find_by_id(cookies[:user_id]) != current_user
       redirect_to root_path
     end
+  end
   end
 end
