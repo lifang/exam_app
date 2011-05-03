81e327c198e2249348d5564cc715a269af38ee70
hejianfeng <marshjf5@126.com>
2011-05-03 17:36:16 +0800

    hejianfeng

--- config/routes.rb
+++ config/routes.rb
@@ -75,10 +75,11 @@ ExamApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "sessions#new"
+ 
   # See how all your routes lay out with "rake routes"
-
+root :to => "sessions#new"
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+ 
