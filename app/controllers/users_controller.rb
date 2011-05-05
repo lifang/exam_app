8d85a14ea8dfd1ea4b38c4d0e426b499b3433b81
hejianfeng <marshjf5@126.com>
2011-05-05 16:49:45 +0800

    hejianfeng

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,11 +47,30 @@ class UsersController < ApplicationController
     render :inline => session[:proof_code]
   end
   def edit
-    @user= User.find(params[:id])
+   # @user= User.find(params[:id])
   end
+  #  def update
+  #    @user = User.find(params[:id])
+  #    @user.update_attributes(params[:user])
+  #    redirect_to @user
+  #  end
+  #  def update
+  #    @user = User.find(params[:id])
+  #    respond_to do |format|
+  #      if @user.update_attributes(params[:user])
+  #        format.html { redirect_to(@user) }
+  #        format.xml  { head :ok }
+  #      else
+  #        format.html { render :action => "edit" }
+  #        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+  #      end
+  #    end
+  #  end
   def update
-    @user = User.find(params[:id])
-    @user.update_attributes(params[:user])
+    @user= User.find(params[:id])
+    puts"#{@user.email}``````````````````````````````````````````````````````````````````"
+    @user.update_attributes(:email=>params[:email])
+   puts"#{@user.email}``````````````````````````````````````````````````````````````````"
     redirect_to @user
   end
 end
