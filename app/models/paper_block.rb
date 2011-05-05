1e0e332228e892069ea4c77560017771782caca8
liyulong <l_y_l12@126.com>
2011-05-05 17:25:35 +0800

    liyulong

--- app/models/paper_block.rb
+++ app/models/paper_block.rb
@@ -1,10 +1,3 @@
 class PaperBlock < ActiveRecord::Base
-<<<<<<< HEAD
-=======
-   belongs_to :paper
 
-   BLOCK_TYPE = {:SINGLE_CHOSE => 0, :MORE_CHOSE =>1, :JUDGE => 2, :SINGLE_CALK => 3,
-                   :MORE_CALK => 4, :COLLIGATION => 5, :CHARACTER => 6 }
-                   #0 单选题； 1 多选题；2 判断题；3 填空题； 4 完型填空题； 5 综合题； 6 简答题
->>>>>>> 6ca3242db79576ca481c7c707850f32697dbf46a
 end
