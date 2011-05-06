1c43dc230705b36dd784a9aee450a3c13b9f152a
hejianfeng <marshjf5@126.com>
2011-05-06 17:03:46 +0800

    hejianfeng

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,25 +50,11 @@ class UsersController < ApplicationController
     @user= User.find(params[:id])
   end
 
-
-
-  #  def update
-  #    @user = User.find(params[:id])
-  #    respond_to do |format|
-  #      if @user.update_attributes(params[:user])
-  #        format.html { redirect_to(@user) }
-  #        format.xml  { head :ok }
-  #      else
-  #        format.html { render :action => "edit" }
-  #        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-  #      end
-  #    end
-  #  end
-#  def update
-#    @user= User.find(params[:user_email])
-#    @user.update_attributes(params[:user])
-#    redirect_to @user
-#  end
+  def update
+    @user= User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to @user
+  end
 
 end
 
