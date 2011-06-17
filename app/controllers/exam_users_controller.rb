83653c76a93c332f1c4688b73465c96a61e761ff
qianjun <495126111@qq.com>
2011-06-17 11:39:52 +0800

    qianjun

--- app/controllers/exam_users_controller.rb
+++ app/controllers/exam_users_controller.rb
@@ -1,6 +1,5 @@
 class ExamUsersController < ApplicationController
-  require 'rexml/document'
-  include REXML
+
   def create_exam_user  #单个添加考生
     @examination = Examination.find(params[:examination_id].to_i)
     @user=User.find_by_sql("select * from users u where u.name='#{params[:exam_user_infoname]}' and u.email='#{params[:exam_user_infoemail]}' ")
@@ -133,7 +132,7 @@ class ExamUsersController < ApplicationController
     @exam_user=ExamUser.find_by_user_id(cookies[:user_id])
     sql = ExamUser.generate_result_sql
     sql += " and us.id=#{cookies[:user_id]} "
-    @results=Examination.paginate_by_sql(sql,:per_page =>5, :page => params[:page])
+    @results=Examination.paginate_by_sql(sql,:per_page =>10, :page => params[:page])
   end
   def search
     session[:start_at] = nil
@@ -155,23 +154,5 @@ class ExamUsersController < ApplicationController
     @results = Examination.paginate_by_sql(sql, :pre_page => 1, :page => params[:page])
     render "my_results"
   end
-  def show
-    exam=ExamUser.find_by_user_id_and_examination_id(cookies[:user_id],params[:id])
-    answer=File.open("#{Rails.root}/public/result/#{exam.id}.xml")
-    @doc=Document.new(answer).root
-    file = File.open("#{Constant::PAPER_PATH}/#{exam.paper_id}.xml")
-    @xml=Document.new(file).root
-    @xml.elements["blocks"].each_element do  |block|
-      block.elements["problems"].each_element do |problem|
-        problem.elements["questions"].each_element do |question|
-          @doc.elements["paper"].elements["questions"].each_element do |element|
-            if element.attributes["id"]==question.attributes["id"]
-              question.add_attribute("user_answer","#{element.elements["answer"].text}")
-                question.add_attribute("user_score","#{element.attributes["score"]}")
-            end    
-          end
-        end
-      end
-    end
-  end
+
 end
