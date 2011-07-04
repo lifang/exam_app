fbccbff3c01e31bf5cbce335b60903b8000cb0e9
lifang <santhe_2004@126.com>
2011-07-04 10:51:11 +0800

    create paper_blocks spec --by lifang

--- spec/controllers/paper_blocks_controller_spec.rb
+++ spec/controllers/paper_blocks_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe PaperBlocksController do
+  it "should load create problem" do
+    post :load_create_problem, :paper_id => "1", :block_id => "32", :type => "create"
+    assigns[:xml].to_s.should_not == ""
+    response.should render_template("new_question")
+  end
+
+  it "should load mavin problem" do
+    post :load_create_problem, :paper_id => "1", :block_id => "32", :type => "mavin"
+    assigns[:xml].to_s.should_not == ""
+    response.should render_template("mavin_question")
+  end
+
+  it "should load edit problem" do
+    post :load_edit_problem, :paper_id => "1", :block_id => "32", :problem_id => "70"
+    assigns[:xml].to_s.should_not == ""
+    response.should render_template("edit_problem")
+  end
+
+  it "should load single choose" do
+    post :choose_type, :id => "32", :paper_id => "1", :question_type => "0"
+    response.should render_template("single_choose")
+  end
 
 end
