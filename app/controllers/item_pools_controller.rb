e2eff01ccc30e9eaa154c5d0d1cf43d471c204d8
liyulong <657433887@qq.com>
2011-07-29 12:41:09 +0800

    lyl

--- app/controllers/item_pools_controller.rb
+++ app/controllers/item_pools_controller.rb
@@ -1,7 +1,13 @@
 class ItemPoolsController < ApplicationController
 
-  def index
-    
-  end
+
+    def index
+      @problems = Problem.search_mothod( nil, nil, nil, nil, 10, params[:page])
+    end
+
+    def index_search
+      #@problems = Problem.search_mothod( nil, nil, nil, nil, 10, params[:page])
+      render 'index'
+    end
 
 end
