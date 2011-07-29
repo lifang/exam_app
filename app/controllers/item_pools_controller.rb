d646556d329595f054a06d9369bc377d22de0eef
qianjun <495126111@qq.com>
2011-07-29 13:16:45 +0800

    qianjun

--- app/controllers/item_pools_controller.rb
+++ app/controllers/item_pools_controller.rb
@@ -1,3 +1,12 @@
 class ItemPoolsController < ApplicationController
 
+
+ def create_paper
+    @paper=Paper.create(:creater_id=>cookies[:user_id],:title=>params[:title].strip,
+      :description=>params[:description].strip, :category_id => params[:category])
+    category = Category.find(params[:category].to_i)
+    @paper.create_paper_url(@paper.xml_content({"category_name" => category.name}), "papers", "xml") unless category.nil?
+    redirect_to "/item_pools/#{@paper.id}/new_step_two"
+  end
+
 end
