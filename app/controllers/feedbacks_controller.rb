1e1e2e665cc0d74644043e2285e2f13a82da1dc9
liyulong <657433887@qq.com>
2011-08-26 11:11:04 +0800

    lyl

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,12 +3,6 @@ class FeedbacksController < ApplicationController
     @feedbacks=Feedback.find_by_sql("select * from feedbacks f order by f.answer and f.created_at")
   end
 
-  def create
-    @feedback=Feedback.new(:description=>params[:feedback][:description],:answer=>params[:feedback][:answer],:user_id=>"#{cookies[:user_id]}")
-    if @feedback.save
-      redirect_to "/feedbacks"
-    end
-  end
 
   def edit
     @feedback = Feedback.find(params[:id])
