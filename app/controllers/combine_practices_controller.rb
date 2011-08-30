7e4829c016a342d5ad0526d11df69548451cf519
liyulong <657433887@qq.com>
2011-08-30 13:23:10 +0800

    lyl

--- app/controllers/combine_practices_controller.rb
+++ app/controllers/combine_practices_controller.rb
@@ -1,7 +1,7 @@
 class CombinePracticesController < ApplicationController
   
   def index
-    
+     @combine_practices=Examination.find_by_sql("select * from examinations where types in (2,3,4,5,6)")
   end
 
 end
