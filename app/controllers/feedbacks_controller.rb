a29b4669ec88015a50d1ccd12c07da83fe400828
lifang <santhe_2004@126.com>
2011-09-09 17:15:08 +0800

    update detial

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class FeedbacksController < ApplicationController
   def index
     @feedbacks=Feedback.paginate_by_sql("select * from feedbacks f order by f.status,  f.created_at",:per_page =>10, :page => params[:page])
@@ -6,6 +7,8 @@ class FeedbacksController < ApplicationController
 
   def edit
     @feedback = Feedback.find(params[:id])
+    @question = Question.find(@feedback.question_id)
+    @problem = Problem.find(@question.id)
   end
 
   def update
