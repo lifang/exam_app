75e81ad9d3bd75c8a5c27607a57e07b25c1a8edc
xuqiyong <jeffrey6052@163.com>
2011-09-09 17:37:34 +0800

    jeffrey

--- app/helpers/constant.rb
+++ app/helpers/constant.rb
@@ -28,7 +28,7 @@ module Constant
   #项目文件目录
   PUBLIC_PATH = "#{Rails.root}/public"
   #前台项目文件目录
-  FRONT_PUBLIC_PATH = "E:/gankao/public"
+  FRONT_PUBLIC_PATH = "D:/gankao/public"
   #试卷生成路径
   PAPER_PATH = "#{PUBLIC_PATH}/papers"
   #试卷服务器访问路径
