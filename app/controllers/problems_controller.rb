fe8b7b3c7797be26511a9c8ad48fec12f7813d74
xuqiyong <jeffrey6052@163.com>
2011-10-18 16:12:25 +0800

    jeffrey

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -126,8 +126,7 @@ class ProblemsController < ApplicationController
   def create_xml(problem, score_arr)
     #更新试卷xml
     url = File.open "#{Constant::PAPER_PATH}/#{params[:problem][:paper_id].to_i}.xml"
-    doc = problem.create_problem_xml(Problem.open_xml(url), params[:problem][:block_id],
-      {:score => score_arr})
+    doc = problem.create_problem_xml(Problem.open_xml(url), params[:problem][:block_id],nil,{:score => score_arr})
     Problem.write_xml(url, doc)
   end
 
