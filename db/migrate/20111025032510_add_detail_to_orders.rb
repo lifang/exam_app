8523827c9bb068af3ba0f683bf0824fc97f6918a
lifang <santhe_2004@126.com>
2011-11-01 16:43:56 +0800

    update detial

--- db/migrate/20111025032510_add_detail_to_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddDetailToOrders < ActiveRecord::Migration
-  def self.up
-    add_column :orders, :out_trade_no, :string
-  end
-
-  def self.down
-    remove_column :orders, :out_trade_no
-  end
-end
