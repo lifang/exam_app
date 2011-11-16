f0b2c50a0ff406fcc642896d081e1a08f6c77ad6
lifang <santhe_2004@126.com>
2011-11-16 12:47:30 +0800

    add db

---
+++ db/migrate/20111116032307_add_detial_to_problems.rb
@@ -0,0 +1,13 @@
+class AddDetialToProblems < ActiveRecord::Migration
+  def self.up
+    add_column :problems, :question_type, :integer
+    add_column :problems, :course_id, :integer
+    add_index :problems,:question_type
+    add_index :problems,:status
+    add_index :problems,:course_id
+  end
+
+  def self.down
+    remove_column :problems, :is_in_title
+  end
+end
