77bf6cd956db1fbf6cfb0aff58489a1359cb846f
qianjun <er6788@126.com>
2011-12-12 16:32:39 +0800

    qianjun

---
+++ db/migrate/20111229044358_add_detail_to_report_errors.rb
@@ -0,0 +1,10 @@
+class AddDetailToReportErrors < ActiveRecord::Migration
+  def self.up
+    add_column :report_errors, :question_id, :integer
+    add_column :report_errors, :status, :bolean
+  end
+
+  def self.down
+    drop_table :report_errors
+  end
+end
