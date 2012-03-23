08465210306942be868ccaf56f99c1bc2771fdb2
lifang <santhe_2004@126.com>
2012-03-23 10:19:40 +0800

    update detial

--- db/migrate/20120324052230_add_category_id_to_feedbacks.rb
+++ db/migrate/20120324052230_add_category_id_to_feedbacks.rb
@@ -1,9 +1,12 @@
 class AddCategoryIdToFeedbacks < ActiveRecord::Migration
   def self.up
+    remove_column :feedbacks, :question_id
     add_column :feedbacks, :category_id, :integer
+    add_index :feedbacks, :category_id
   end
 
   def self.down
     remove_column :feedbacks, :category_id
+    add_column :feedbacks, :question_id, :integer
   end
 end
